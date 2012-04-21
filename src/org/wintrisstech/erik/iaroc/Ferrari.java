c49a62d0d3a69b6230dadbc2f3b4f5de46cf5483
vic <vic@10.0.1.37>
2012-04-21 14:49:31 -0700

    asdtuykjgfsaZxdfghkefwsaszxckvlyjhgszxckvjh

---
+++ src/org/wintrisstech/erik/iaroc/Ferrari.java
@@ -0,0 +1,170 @@
+package org.wintrisstech.erik.iaroc;
+
+import android.os.SystemClock;
+import android.speech.tts.TextToSpeech;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.exception.ConnectionLostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.wintrisstech.irobot.ioio.IRobotCreateAdapter;
+import org.wintrisstech.irobot.ioio.IRobotCreateInterface;
+import org.wintrisstech.sensors.UltraSonicSensors;
+
+/**
+ * A Ferrari is an implementation of the IRobotCreateInterface.
+ *
+ * @author Erik
+ */
+public class Ferrari extends IRobotCreateAdapter implements Runnable
+{
+    private static final String TAG = "Ferrari";
+    private final UltraSonicSensors ultraSonicSensors;
+    private final Dashboard dashboard;
+    /*
+     * The maze can be thought of as a grid of quadratic cells, separated by
+     * zero-width walls. The cell width includes half a pipe diameter on each
+     * side, i.e the cell edges pass through the center of surrounding pipes.
+     * <p> Row numbers increase northward, and column numbers increase eastward.
+     * <p> Positions and direction use a reference system that has its origin at
+     * the west-most, south-most corner of the maze. The x-axis is oriented
+     * eastward; the y-axis is oriented northward. The unit is 1 mm. <p> What
+     * the Ferrari knows about the maze is:
+     */
+    private final static int NUM_ROWS = 12;
+    private final static int NUM_COLUMNS = 4;
+    private final static int CELL_WIDTH = 712;
+    /*
+     * State variables:
+     */
+    private int speed = 300; // The normal speed of the Ferrari when going straight
+    // The row and column number of the current cell. 
+    private int row;
+    private int column;
+    private boolean running = true;
+    private final static int SECOND = 1000; // number of millis in a second
+    private final static int REDBUOY = 248; // number of millis in a second
+    private final static int GREENBUOY = 244; // number of millis in a second
+    private final static int ALL = 254; // number of millis in a second
+    private final static int NONE = 255; // number of millis in a second
+
+    /**
+     * Constructs a Ferrari, an amazing machine!
+     *
+     * @param ioio the IOIO instance that the Ferrari can use to communicate
+     * with other peripherals such as sensors
+     * @param create an implementation of an iRobot
+     * @param dashboard the Dashboard instance that is connected to the Ferrari
+     * @throws ConnectionLostException
+     */
+    public Ferrari(IOIO ioio, IRobotCreateInterface create, Dashboard dashboard) throws ConnectionLostException
+    {
+        super(create);
+        ultraSonicSensors = new UltraSonicSensors(ioio);
+        this.dashboard = dashboard;
+    }
+
+    /**
+     * Main method that gets the Ferrari running.
+     *
+     */
+    public void run()
+    {
+        dashboard.log("Christian's program v.4");
+        try
+        {
+            while (true)
+            {
+                readSensors(SENSORS_GROUP_ID6);
+                if (isBumpLeft() || isBumpRight())
+                {
+                   driveDirect(-150, -100);
+                   SystemClock.sleep(3000);
+                }
+                int ir = getInfraredByte();
+                if (ir == NONE)
+                {
+                    driveDirect(-100, 100);
+                } else if (ir == GREENBUOY)
+                {
+                    driveDirect(50, 100);
+                } else if (ir == REDBUOY)
+                {
+                    driveDirect(100, 50);
+                } else if (ir == ALL)
+                {
+                    driveDirect(500, 500);
+                } else
+                {
+                    driveDirect(100, 100);
+                }
+            }
+        } catch (Exception e)
+        {
+            dashboard.log(e.getMessage());
+        }
+        try
+        {
+            driveDirect(0, 0);
+        } catch (ConnectionLostException ex)
+        {
+            dashboard.log("DEAD");
+        }
+
+    }
+
+    /**
+     * Closes down all the connections of the Ferrari, including the connection
+     * to the iRobot Create and the connections to all the sensors.
+     */
+    public void shutDown()
+    {
+        closeConnection(); // close the connection to the Create
+        ultraSonicSensors.closeConnection();
+    }
+
+    //// Methods made public for the purpose of the Dashboard ////
+    /**
+     * Gets the left distance to the wall using the left ultrasonic sensor
+     *
+     * @return the left distance
+     */
+    public int getLeftDistance()
+    {
+        return ultraSonicSensors.getLeftDistance();
+    }
+
+    /**
+     * Gets the front distance to the wall using the front ultrasonic sensor
+     *
+     * @return the front distance
+     */
+    public int getFrontDistance()
+    {
+        return ultraSonicSensors.getFrontDistance();
+    }
+
+    /**
+     * Gets the right distance to the wall using the right ultrasonic sensor
+     *
+     * @return the right distance
+     */
+    public int getRightDistance()
+    {
+        return ultraSonicSensors.getRightDistance();
+    }
+
+    /**
+     * Checks if the Ferrari is running
+     *
+     * @return true if the Ferrari is running
+     */
+    public synchronized boolean isRunning()
+    {
+        return running;
+    }
+
+    private synchronized void setRunning(boolean b)
+    {
+        running = false;
+    }
+}
